c2aae1b23dd6b9f86d15b3af71199cc8022eeb60
Najat Khoudrouf <nkhoudro@e2r10p1.1337.ma>
2023-11-20 16:14:20 +0100

    ex00

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,60 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+    std::cout << "Default constracter called"<< std::endl;
+}
+ClapTrap::~ClapTrap()
+{
+    std::cout << "Deconstracter called"<< std::endl;
+}
+ClapTrap::ClapTrap(ClapTrap &clapTrap)
+{
+    std::cout << "Copy constracter called"<< std::endl;
+    *this = clapTrap;
+}
+ClapTrap::ClapTrap(std::string name)
+{
+    std::cout << "Constracter paramitrize called"<< std::endl;
+    this->Name = name;
+    this->HitPoints = 10;
+    this->EnerguPoint = 10;
+    this->AttackDamage = 0;
+}
+ClapTrap &ClapTrap::operator=(const  ClapTrap &clapTrap)
+{
+    this->HitPoints = clapTrap.HitPoints;
+    this->EnerguPoint = clapTrap.EnerguPoint;
+    this->AttackDamage = clapTrap.AttackDamage;
+    return (*this);
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+    if (this->EnerguPoint <= 0)
+    {
+        std::cout << "ClapTrap " << this->Name << " no energy left" << std::endl;
+        return ;
+    }
+    if (this->HitPoints <= 0)
+    {
+        std::cout << "ClapTrap " << this->Name << "  no  health left" << std::endl;
+        return ;
+    }
+    this->EnerguPoint -= 1;
+    std::cout << "ClapTrap " << this->Name << " attack " << target << ", causing " << this->AttackDamage << " points of damage!" << std::endl;
+}
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    this->HitPoints -= amount;
+    std::cout << "ClapTrap " << this->Name << " take " << amount << " points of damage!" << std::endl;
+}
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    if (this->EnerguPoint > 0)
+    {
+        this->HitPoints += amount;
+        std::cout << "ClapTrap " << this->Name << " be repaired " << amount << " points of damage!" << std::endl;
+        this->EnerguPoint -= 1;
+    }
+}
